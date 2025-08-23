1b4f1d89896ff91ebe0bb2331baba67692a82ddc
aaryanwadhawan7 <aaryanwadhawam339@gmail.com>
2025-08-23 13:11:28 +0530

    Added CONSTANTS file to check how the application works

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import Navbar from '~/Components/Navbar';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,11 +10,15 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar></Navbar>
+
     <section className = "main-section">
       <div className = "page-heading">
         <h1>Track your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
     </section>
+
+
   </main>;
 }
