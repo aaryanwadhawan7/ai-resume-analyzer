ac4c5d31ad851e93084d965e296fac6cc6a558f8
aaryanwadhawan7 <aaryanwadhawam339@gmail.com>
2025-08-23 12:22:19 +0530

    First commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,19 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
+
+export default function Home() {
+  return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className = "main-section">
+      <div className = "page-heading">
+        <h1>Track your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>;
+}
